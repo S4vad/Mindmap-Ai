bc9635cb33f0939c0663f6c5a4c26cc0848e7401
savad k <124030718+S4vad@users.noreply.github.com>
2025-09-18 17:36:35 +0530

    deploy edit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,7 +10,7 @@ import {
 } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import Card1 from "../components/cards/Card1";
-import Card2 from "../components/cards/card2";
+import Card2 from "../components/cards/Card2";
 import Card3 from "../components/cards/Card3";
 import Card4 from "../components/cards/Card4";
 import { motion } from "framer-motion";
